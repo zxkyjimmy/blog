e3b772cb394ce7906e3aae8f658ed9950933f303
Yen-Chi Chen <zxkyjimmy@gmail.com>
2022-09-14 19:28:40 +0800

    first deploy

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+ const config = {
+  base: '/blog',  // 儲存庫名稱
+  lang: 'zh-Hant-TW',
+  title: 'VitePress',
+  description: 'Just playing around.'
+}
+
+export default config
